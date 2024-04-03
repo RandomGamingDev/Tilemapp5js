706616a19e87c666620be40c7ce2d065564a4936
RandomGamingDev <ethanshiyi1123@gmail.com>
2024-04-03 10:36:35 -0400

    added documentation

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Tilemapp5js
-A tilemap library for p5 js
+A tilemap library for p5.js
 
 To use the library simply create a new instance of the pixy class with the position, size, resolution, image and tile list, (optional) offset (manages where the tile rendering starts from and is by default [0, 0]) (they're all arrays with 2 elements, except for the last 2, with the 2nd last being an image and the last being a 2d array with each array of the main array having a length of 4) (put the p5.js instance that you want to use in the last parameter slot if you want to use p5.js instances) and then just change the Pixy.pixels array and execute Tilemap.display() in order to display!
 
 To use it you can simply include https://cdn.jsdelivr.net/gh/RandomGamingDev/Tilemapp5js/tilemap.js in your HTML file! If you want to you can also just download the file and include it in your HTML file that way.
 
 btw stuff updates so remember to specify a version/commit for your library if you want to use a link and don't want your code to automatically update to the newest version of the library
+
+
---
+++ documentation/tilemap-demonstration.js
@@ -0,0 +1,48 @@
+new p5(p => {
+    let canvas;
+    let texture_atlas;
+    let tilemap;
+
+    p.preload = function () {
+        texture_atlas = p.loadImage("documentation/tilemap.png");
+    }
+
+    p.setup = function () {
+        // Create the canvas to draw onto
+        canvas = p.createCanvas(700, 400);
+        canvas.parent("sketch-holder");
+        p.fill(255);
+        p.textFont("Arial");
+
+        // Create the tilemap (works in instance mode like this :D)
+        tilemap = new Tilemap([0, 0], [p.width, p.height], [28, 16], texture_atlas, [], [0, 0], p);
+        // Create a list containing the tile coordinates and size
+        for (let i = 0; i < 20; i++)
+            for (let j = 0; j < 20; j++)
+                tilemap.tiles.push([j * 64, i * 64, 64, 64])
+
+        // Load the tilemap from the Tiled file
+        const tiled_tilemap = 
+            fetch("documentation/example-tilemap.json", {cache: "no-cache"})
+                .then(res => res.json())
+                .then(json => {
+                    const data = json["layers"][0]["data"];
+                    for (let i = 0; i < tilemap.res[0]; i++)
+                        for (let j = 0; j < tilemap.res[1]; j++)
+                            tilemap.tilemap[i][j] = data[j * tilemap.res[0] + i];
+                });
+
+    };
+
+    p.draw = function () {
+        p.background(0);
+
+        // Display the tilemap and measure the time
+        const start = performance.now();
+        tilemap.display();
+        const render_time = performance.now() - start;
+
+        // Draw text showing how much time it took to render
+        p.text(`Render Time (ms): ${ render_time }`, 10, 20);
+    };
+});
